07e9f6646e2b03112d677444a399cea530d97cb4
eugene1smith <steinway89@gmail.com>
2015-10-02 00:16:13 +0300

    Создал общую фикстуру

---
+++ conftest.py
@@ -0,0 +1,15 @@
+__author__ = 'Eugene'
+
+from fixture.application import Application
+import pytest
+
+
+
+
+@pytest.fixture(scope = "session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
-from fixture.application import Application
-import pytest
+
 from model.group import group
 
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(group(name="Name", header="Head", footer="Footer"))
+    app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
